43535e064d28256933fea0862ecf999754d9b16c
simrat39 <simrats169169@gmail.com>
2020-07-16 15:06:23 -0700

    Improve the snackbar
    Make it floating and rounded

--- lib/widgets/suckless_snackbar.dart
+++ lib/widgets/suckless_snackbar.dart
@@ -7,5 +7,7 @@ Widget SucklessSnackbar(BuildContext context, String text) {
       style: TextStyle(color: Theme.of(context).textTheme.headline6.color),
     ),
     backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
+    behavior: SnackBarBehavior.floating,
   );
 }
